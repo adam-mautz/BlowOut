68dfb74c35a1d9398bc86e2ec2f4f2c5c8b847bf
Adam.mautz <Adam.mautz@gmail.com>
2019-11-11 14:01:16 -0700

    Add project files.

---
+++ BlowOut/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿//Adam Mautz
+//Changed some of the text on the index page to reflext checkpoint 1
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
